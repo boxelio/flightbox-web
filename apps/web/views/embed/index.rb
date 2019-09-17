40a9dbbfd937c9bbcd0e2c871d9db37926566586
Kevin Elliott <kevin@welikeinc.com>
2019-09-17 00:18:25 -0700

    Add 978

--- apps/web/views/embed/index.rb
+++ apps/web/views/embed/index.rb
@@ -7,6 +7,7 @@ module Web
         def iframe_src
           case source
           when 'skyview1090' then '/dump1090-fa'
+          when 'skyview978' then '/skyview978'
           when 'tar1090' then '/tar1090'
           when 'timelapse1090' then '/timelapse1090'
           end
